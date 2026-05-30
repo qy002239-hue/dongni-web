c9da461cb8658452b99e0b968acbaf0e7aaccb33
qy002239-hue <qy002239@gmail.com>
2026-05-30 17:44:42 +0800

    Update Onboarding.jsx

--- src/Onboarding.jsx
+++ src/Onboarding.jsx
@@ -51,7 +51,7 @@ export default function Onboarding({ onDone }) {
         position: "fixed", inset: 0, margin: 0, color: "#e2e8f0",
         overflow: "hidden", userSelect: "none", textAlign: "center",
         display: "flex", flexDirection: "column", justifyContent: "center",
-        backgroundImage: " url('/ocean.jpg')",
+        backgroundImage: "url('/ocean.jpg.jpg')",
         backgroundSize: "cover", backgroundPosition: "center bottom", backgroundRepeat: "no-repeat",
       }}
       onMouseDown={handlePointerDown} onMouseUp={handlePointerUp}
