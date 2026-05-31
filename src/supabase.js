aaa325d3eba301ab3c2530daa4d20a9b47bec57e
qy002239-hue <qy002239@gmail.com>
2026-05-31 11:59:51 +0800

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,1 +1,11 @@
 
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://lssmeqgzgkibcngrxpgg.supabase.co'
+
+const supabaseAnonKey = '先留空'
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+)
