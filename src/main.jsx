6aff7561b38fded007953b45a1f1531acecfc821
qy002239-hue <qy002239@gmail.com>
2026-06-30 20:08:59 +0800

    fix: restore production entrypoint to match main

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import TestReception from './TestReception.jsx'
-
-const path = window.location.pathname;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {path === '/test-reception' ? <TestReception /> : <App />}
+    <App />
   </StrictMode>,
 )
